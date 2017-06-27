7fc248c8a9960b82ba8cb2804f0c5c75c0c59002
Douglas Logan <dlogan@cyberninjas.com>
2017-06-27 00:48:03 -0400

    Add variable checking for script to run.

--- bin/master_pack.js
+++ bin/master_pack.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 const lib = require('../lib/');
 
+var argv = require('minimist')(process.argv.slice(2));
+
+if(argv['target'])
+    process.env.DEPLOYMENT_SOURCE=argv['target']
+
+if(!process.env.DEPLOYMENT_SOURCE) {
+    console.log("ENV variable DEPLOYMENT_SOURCE must be set or a value passed for --target")
+    process.exit(1)
+}
+
+var fs = require('fs');
+if (!fs.existsSync(process.env.DEPLOYMENT_SOURCE)) {
+    console.log("Target folder set with ENV DEPLOYMENT_SOURCE or --target does not exist");
+    process.exit(1);
+}
 lib.createDistribution();
 
