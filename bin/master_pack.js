b688407eae50641d2249ab20a9efe0d5dc6de432
Douglas Logan <dlogan@cyberninjas.com>
2017-06-27 08:00:33 -0400

    Change rmdir to emptydir to easy running.

--- bin/master_pack.js
+++ bin/master_pack.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
-const lib = require('../lib/');
+const lib   = require('../lib/');
+const path  = require('path');
 
 var argv = require('minimist')(process.argv.slice(2));
 
 if(argv['target'])
-    process.env.DEPLOYMENT_SOURCE=argv['target']
+    process.env.DEPLOYMENT_SOURCE=argv['target'];
 
 if(!process.env.DEPLOYMENT_SOURCE) {
     console.log("ENV variable DEPLOYMENT_SOURCE must be set or a value passed for --target")
     process.exit(1)
 }
+
+process.env.DEPLOYMENT_SOURCE=path.resolve(process.env.DEPLOYMENT_SOURCE);
 
 var fs = require('fs');
 if (!fs.existsSync(process.env.DEPLOYMENT_SOURCE)) {
