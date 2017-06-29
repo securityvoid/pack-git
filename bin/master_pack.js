e9f32bb6a7bcfca8c1ee7324d40c659b058782f8
Douglas Logan <dlogan@cyberninjas.com>
2017-06-29 11:56:08 -0400

    Refactor/rename variables to avoid collisions and prefeix with MASTERPACK.

--- bin/master_pack.js
+++ bin/master_pack.js
@@ -5,17 +5,17 @@ const path  = require('path');
 var argv = require('minimist')(process.argv.slice(2));
 
 if(argv['target'])
-    process.env.DEPLOYMENT_SOURCE=argv['target'];
+    process.env.MASTERPACK_SOURCE=argv['target'];
 
-if(!process.env.DEPLOYMENT_SOURCE) {
+if(!process.env.MASTERPACK_SOURCE) {
     console.log("ENV variable DEPLOYMENT_SOURCE must be set or a value passed for --target")
     process.exit(1)
 }
 
-process.env.DEPLOYMENT_SOURCE=path.resolve(process.env.DEPLOYMENT_SOURCE);
+process.env.MASTERPACK_SOURCE=path.resolve(process.env.MASTERPACK_SOURCE);
 
 var fs = require('fs');
-if (!fs.existsSync(process.env.DEPLOYMENT_SOURCE)) {
+if (!fs.existsSync(process.env.MASTERPACK_SOURCE)) {
     console.log("Target folder set with ENV DEPLOYMENT_SOURCE or --target does not exist");
     process.exit(1);
 }
