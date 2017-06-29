3f5fda1b9918ff9e5dbdf90cd9ed90adfdc1284d
Douglas Logan <dlogan@cyberninjas.com>
2017-06-29 17:18:27 -0400

    Refactor/Rename from Master-pack to pack-git, since this will work for any branch.

--- bin/pack_git.js
+++ bin/pack_git.js
@@ -5,17 +5,17 @@ const path  = require('path');
 var argv = require('minimist')(process.argv.slice(2));
 
 if(argv['target'])
-    process.env.MASTERPACK_SOURCE=argv['target'];
+    process.env.PACKGIT_SOURCE=argv['target'];
 
-if(!process.env.MASTERPACK_SOURCE) {
+if(!process.env.PACKGIT_SOURCE) {
     console.log("ENV variable DEPLOYMENT_SOURCE must be set or a value passed for --target")
     process.exit(1)
 }
 
-process.env.MASTERPACK_SOURCE=path.resolve(process.env.MASTERPACK_SOURCE);
+process.env.PACKGIT_SOURCE=path.resolve(process.env.PACKGIT_SOURCE);
 
 var fs = require('fs');
-if (!fs.existsSync(process.env.MASTERPACK_SOURCE)) {
+if (!fs.existsSync(process.env.PACKGIT_SOURCE)) {
     console.log("Target folder set with ENV DEPLOYMENT_SOURCE or --target does not exist");
     process.exit(1);
 }
